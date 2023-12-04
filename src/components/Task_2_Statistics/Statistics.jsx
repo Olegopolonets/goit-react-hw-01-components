ad43def8362878b24f6dd9b2da8c5d0bcd6a402a
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-04 22:53:28 +0200

    add css

--- src/components/Task_2_Statistics/Statistics.jsx
+++ src/components/Task_2_Statistics/Statistics.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import s from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <section className="statistics">
-        <h2 className="title">{title}</h2>
+      <section className={s.statistics}>
+        <h2 className={s.title}>{title}</h2>
 
-        <ul className="stat-list">
+        <ul className={s.statList}>
           {stats.map(({ id, label, percentage }) => {
             return (
-              <li key={id} className="item">
+              <li key={id} className={s.item}>
                 <span className="label">{label}</span>
                 <span className="percentage">{percentage}</span>
               </li>
