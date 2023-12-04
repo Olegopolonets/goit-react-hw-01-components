a0de625ebf2b292963ffc63a7bff8678274bdbef
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-04 23:12:36 +0200

    add className={s.friendsList}

--- src/components/Task_3_FriendsList/FriendsList.jsx
+++ src/components/Task_3_FriendsList/FriendsList.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import s from './FriendsList.module.css';
 
 export const FriendsList = ({ friends }) => {
   return (
     <>
-      <section className="section-list-friends">
-        <ul className="friend-list">
+      <section className={s.section}>
+        <ul className={s.friendsList}>
           {friends.map(({ avatar, name, isOnline, id }) => {
             return (
-              <li key={id} className="friend-item">
-                <span className="friend-status">{isOnline}</span>
+              <li key={id} className={s.friendsItem}>
+                <span className={s.friendsStatus}>{isOnline}</span>
                 <img
                   src={avatar}
                   alt="friend avatar"
-                  className="friend-avatar"
+                  className={s.friendsAvatar}
                 />
-                <p className="friend-name">{name}</p>
+                <p className={s.friendsName}>{name}</p>
               </li>
             );
           })}
