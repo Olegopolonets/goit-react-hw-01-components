17bbd55b58b00727617c795069dc5cd64ac464fc
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-04 22:59:12 +0200

    add title

--- src/components/Task_2_Statistics/Statistics.jsx
+++ src/components/Task_2_Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ export const Statistics = ({ title, stats }) => {
   return (
     <>
       <section className={s.statistics}>
-        <h2 className={s.title}>{title}</h2>
+        {title && <h2 className={s.title}>{title}</h2>}
 
         <ul className={s.statList}>
           {stats.map(({ id, label, percentage }) => {
